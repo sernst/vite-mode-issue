088ca9316f94ec3fe132727a5ed571e17d8f7b7a
Scott Ernst <swernst@gmail.com>
2023-05-27 07:11:47 -0500

    Add NODE_ENV
    Adds .env files with NODE_ENV as recommended. Also,
    included a custom VITE_ENV to demonstrate the .env
    files were sourced as expected and responsible for
    the correction in build mode values.

--- main.js
+++ main.js
@@ -3,4 +3,5 @@ document.querySelector('#app').innerHTML = `
   <p>MODE: ${import.meta.env.MODE}</p>
   <p>DEV: ${import.meta.env.DEV}</p>
   <p>PROD: ${import.meta.env.PROD}</p>
+  <p>VITE_ENV: ${import.meta.env.VITE_ENV}</p>
 `;
--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,9 @@
-export default {
-    base: "./",
-}
+import { defineConfig } from "vite";
+
+export default defineConfig(({ mode}) => {
+  const config = { base: "./", mode, build: {} };
+  if (mode === "development") {
+    config.build.minify = false;
+  }
+  return config;
+});
